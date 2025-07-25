e44dcbefd37323d5c8c7f54ca2ab6df66ebbd6f8
gc1523 <guyconnolly15@gmail.com>
2025-07-25 18:47:45 +0100

    Diffuse materials

--- sphere.h
+++ sphere.h
@@ -8,7 +8,8 @@ class sphere : public hittable {
   public:
     sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
 
-      bool hit(const ray& r, interval ray_t, hit_record& rec) const override {        vec3 oc = center - r.origin();
+      bool hit(const ray& r, interval ray_t, hit_record& rec) const override {        
+        vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius*radius;
@@ -21,7 +22,7 @@ class sphere : public hittable {
 
         // Find the nearest root that lies in the acceptable range.
         auto root = (h - sqrtd) / a;
-        if (root <= ray_t.surrounds(root)) {
+        if (!ray_t.surrounds(root)) {
             root = (h + sqrtd) / a;
             if (!ray_t.surrounds(root))
                 return false;
